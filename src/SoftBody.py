1769d45c83adb92c5bcb5c3c9281d5e88011c033
sharifigiv <76777409+sharifigiv@users.noreply.github.com>
2023-04-28 15:54:08 +0330

    Update SoftBody

--- src/SoftBody.py
+++ src/SoftBody.py
@@ -1,34 +1,73 @@
 from Body import MDynamics
 from Vector import Vector, rotate
+from Spring import Spring
+import pyray as pr 
 
+pr.init_window(1080, 720, "Soft Body")
 
 class SoftBody:
-    def __init__(self, length, particle_num, first_position):
+    def __init__(self, length: int, particle_column: int, particle_row: int, start_pos: Vector, phy: MDynamics):
+        self.length = length
+        self.k = 50
+        self.damping = 0.4
+
+        self.particle_column = particle_column
+        self.particle_row = particle_row
+
+        self.start_pos = start_pos
+
         self.particles = []
         self.springs = []
 
-        self.spring_length = length
-        self.k = 50
-        self.damping = 0.2
-
-        self.particle_num = particle_num
-        self.first_position = first_position
+        self.phy = phy
 
     def create_particles(self):
-        sides = []
-        new_line = [Vector(self.first_position[0], self.first_position[1]),
-                    Vector(self.first_position[0] + self.spring_length, self.first_position[1] + 0)]
+        i = 1
+        for row in range(self.particle_row):
+            for column in range (self.particle_column):
+                particle = self.phy.circle_rigidbody("C" + str(i), row * self.length + self.start_pos.x, column * self.length + self.start_pos.y, 5, 1)
+                self.particles.append(particle)
+                
+                i += 1
 
-        sides.append(new_line)
-        for side in range(self.particle_num):
-            last_point_2 = sides[len(sides) - 1][1]
-            last_point_1 = sides[len(sides) - 1][0]
+    def create_springs(self):
+        for i in range(len(self.particles)):
+            for j in range(i+1, len(self.particles)):
+                length = ((self.particles[i].position.x - self.particles[j].position.x)**2 + (self.particles[i].position.y - self.particles[j].position.y)**2) ** 0.5
+                self.springs.append(Spring(self.particles[i], self.particles[j], length, self.k, self.damping))
+    
+    def update_springs(self):
+        for s in self.springs:
+            s.update()
 
-            line_last = last_point_2 - last_point_1
-            final_point = line_last + last_point_2
+    def draw_particles(self):
+        for particle in self.particles:
+            # print(par)
+            pr.draw_circle(int(particle.position.x), int(particle.position.y), particle.r, pr.WHITE)
 
-            final_line = rotate(last_point_2, final_point, 1)
-            sides.append([last_point_2, final_line])
-        for i in range(self.particle_num):
-            for j in range(i, self.particle_num + 1):
-                pass
+    def draw_springs(self):
+        for spring in self.springs:
+            pr.draw_line(int(spring.particle1.position.x), int(spring.particle1.position.y), int(spring.particle2.position.x), int(spring.particle2.position.y), pr.WHITE)
+
+p = MDynamics()
+
+s = SoftBody(50, 2, 4, Vector(100, 100), p)
+s.create_particles()
+s.create_springs()
+
+print(len(s.springs))
+
+while not pr.window_should_close():
+    pr.begin_drawing()
+    pr.clear_background(pr.BLACK)
+
+    p.calculate_collisions()
+    p.update_rigid_bodies(0.0009)
+
+    s.update_springs()
+
+    s.draw_particles()
+    s.draw_springs()
+    
+
+    pr.end_drawing()
